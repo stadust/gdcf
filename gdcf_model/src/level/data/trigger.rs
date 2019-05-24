e4f90c0f266cca3a25a74daee01dc44d3faf6823
stadust <43299462+stadust@users.noreply.github.com>
2019-05-24 21:23:54 +0200

    Fixed compilation failure when the serde_support feature is enabled due to missing imports

--- gdcf_model/src/level/data/trigger.rs
+++ gdcf_model/src/level/data/trigger.rs
@@ -1,3 +1,6 @@
+#[cfg(feature = "serde_support")]
+use serde_derive::{Serialize, Deserialize};
+
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 #[cfg_attr(feature = "serde_support", derive(Serialize, Deserialize))]
 pub struct ColorTriggerData {
