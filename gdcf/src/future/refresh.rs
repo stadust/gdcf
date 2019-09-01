291b38ebb1ec68e01c495d560457d2c18a562342
stadust <43299462+stadust@users.noreply.github.com>
2019-09-01 15:54:26 +0200

    Make RefreshCacheFuture hold a complete GDCF (because ProcessRequestFuture needs one for the upgrade process)

--- gdcf/src/future/refresh.rs
+++ gdcf/src/future/refresh.rs
@@ -7,7 +7,7 @@ use crate::{
     cache::{Cache, CacheEntry, CanCache, Store},
     error::{ApiError, GdcfError},
     future::GdcfFuture,
-    Secondary,
+    Gdcf, Secondary,
 };
 use futures::{Async, Future};
 use gdcf_model::{song::NewgroundsSong, user::Creator};
@@ -21,7 +21,7 @@ where
     C: Cache + Store<Creator> + Store<NewgroundsSong> + CanCache<Req>,
 {
     inner: <A as MakeRequest<Req>>::Future,
-    cache: C,
+    gdcf: Gdcf<A, C>,
     cache_key: u64,
 }
 
@@ -31,8 +31,23 @@ where
     A: ApiClient + MakeRequest<Req>,
     C: Cache + Store<Creator> + Store<NewgroundsSong> + CanCache<Req>,
 {
-    pub(crate) fn new(cache: C, cache_key: u64, inner: <A as MakeRequest<Req>>::Future) -> Self {
-        RefreshCacheFuture { inner, cache, cache_key }
+    pub fn gdcf(&self) -> Gdcf<A, C> {
+        self.gdcf.clone()
+    }
+
+    pub fn cache_key(&self) -> u64 {
+        self.cache_key
+    }
+}
+
+impl<Req, A, C> RefreshCacheFuture<Req, A, C>
+where
+    Req: Request,
+    A: ApiClient + MakeRequest<Req>,
+    C: Cache + Store<Creator> + Store<NewgroundsSong> + CanCache<Req>,
+{
+    pub(crate) fn new(gdcf: Gdcf<A, C>, cache_key: u64, inner: <A as MakeRequest<Req>>::Future) -> Self {
+        RefreshCacheFuture { inner, gdcf, cache_key }
     }
 }
 
@@ -46,6 +61,8 @@ where
     type Item = CacheEntry<Req::Result, C::CacheEntryMeta>;
 
     fn poll(&mut self) -> Result<Async<Self::Item>, Self::Error> {
+        let mut cache = self.gdcf.cache();
+
         match self.inner.poll() {
             Ok(Async::NotReady) => Ok(Async::NotReady),
             Err(ref api_error) if api_error.is_no_result() => {
@@ -53,7 +70,7 @@ where
 
                 warn!("Request yielded no result, marking as absent");
 
-                Store::<Req::Result>::mark_absent(&mut self.cache, self.cache_key)
+                Store::<Req::Result>::mark_absent(&mut cache, self.cache_key)
                     .map(|entry_info| Async::Ready(CacheEntry::MarkedAbsent(entry_info)))
                     .map_err(GdcfError::Cache)
             },
@@ -61,22 +78,22 @@ where
             Ok(Async::Ready(response)) =>
                 match response {
                     Response::Exact(what_we_want) =>
-                        self.cache
+                        cache
                             .store(&what_we_want, self.cache_key)
                             .map(|entry_info| Async::Ready(CacheEntry::Cached(what_we_want, entry_info)))
                             .map_err(GdcfError::Cache),
                     Response::More(what_we_want, excess) => {
                         for object in &excess {
                             match object {
-                                Secondary::NewgroundsSong(song) => self.cache.store(song, song.song_id),
-                                Secondary::Creator(creator) => self.cache.store(creator, creator.user_id),
-                                Secondary::MissingCreator(cid) => Store::<Creator>::mark_absent(&mut self.cache, *cid),
-                                Secondary::MissingNewgroundsSong(nid) => Store::<NewgroundsSong>::mark_absent(&mut self.cache, *nid),
+                                Secondary::NewgroundsSong(song) => cache.store(song, song.song_id),
+                                Secondary::Creator(creator) => cache.store(creator, creator.user_id),
+                                Secondary::MissingCreator(cid) => Store::<Creator>::mark_absent(&mut cache, *cid),
+                                Secondary::MissingNewgroundsSong(nid) => Store::<NewgroundsSong>::mark_absent(&mut cache, *nid),
                             }
                             .map_err(GdcfError::Cache)?;
                         }
 
-                        self.cache
+                        cache
                             .store(&what_we_want, self.cache_key)
                             .map(|entry_info| Async::Ready(CacheEntry::Cached(what_we_want, entry_info)))
                             .map_err(GdcfError::Cache)
